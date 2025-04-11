b1056abe67e582d6071e56a197e7012a47b9957b
olga <odubasss@gmail.com>
2025-04-11 21:47:46 +0200

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'a5Cdg8Lofi9uoUwp8IJJcaJuXeseE2xq_fOWSbfxplU';
+const API_KEY = 'WRITE YOUR ACCESS KEY HERE';
 const BASE_URL = 'https://api.unsplash.com/search/photos';
 
 export const fetchImages = async (query, page) => {
